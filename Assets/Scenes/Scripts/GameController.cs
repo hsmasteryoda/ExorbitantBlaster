9edac3341cf16eed1129c7fac0feda33a15df217
uncagedsoccer <46938673+uncagedsoccer@users.noreply.github.com>
2020-04-23 12:57:31 -0500

    Added asteroid movement, fixed shooting, and added gamecontroller

---
+++ Assets/Scenes/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Vector3 spawnValues;
+    public int smallAsteroidCount;
+    public float spawnWait;
+    public float waveWait;
+    public float startWait;
+    public GameObject smallAsteroid;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for(int i = 0; i < smallAsteroidCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(smallAsteroid, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+}
