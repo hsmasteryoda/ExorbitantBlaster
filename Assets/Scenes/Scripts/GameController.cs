8230ea98b8c124e215ec56092b0b7b7d7e2b4dcf
uncagedsoccer <46938673+uncagedsoccer@users.noreply.github.com>
2020-04-25 15:44:11 -0500

    Added score counter and mutiple wave spawns

--- Assets/Scenes/Scripts/GameController.cs
+++ Assets/Scenes/Scripts/GameController.cs
@@ -6,10 +6,19 @@ public class GameController : MonoBehaviour
 {
     public Vector3 spawnValues;
     public int smallAsteroidCount;
-    public float spawnWait;
+    public int bigAsteroidCount;
+    public GameObject bigAsteroid;
+    public float spawnWait1;
+    public float spawnWait2;
+    public float spawnWait3a;
+    public float spawnWait3b;
     public float waveWait;
     public float startWait;
     public GameObject smallAsteroid;
+    public int wave3Count;
+    private bool wave1 = true;
+    private bool wave2 = true;
+    private bool wave3 = true;
 
     void Start()
     {
@@ -19,16 +28,41 @@ public class GameController : MonoBehaviour
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-        while (true)
+        while (wave1 == true)
         {
             for (int i = 0; i < smallAsteroidCount; i++)
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(smallAsteroid, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
+                yield return new WaitForSeconds(spawnWait1);
             }
             yield return new WaitForSeconds(waveWait);
+            wave1 = false;
+
+        }
+        while(wave2 == true)
+        {
+            for (int i = 0; i < bigAsteroidCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(bigAsteroid, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait2);
+            }
+            wave2 = false;
+        }
+        while(wave3 == true)
+        {
+            for (int i = 0; i < wave3Count; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(smallAsteroid, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait3a);
+                Instantiate(bigAsteroid, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait3b);
+            }
         }
     }
 
