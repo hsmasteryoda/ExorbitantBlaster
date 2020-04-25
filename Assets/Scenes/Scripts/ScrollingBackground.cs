bcf8ac821cfa28e4ad2e0abbff25533704310222
uncagedsoccer <46938673+uncagedsoccer@users.noreply.github.com>
2020-04-25 12:32:39 -0500

    Added Scrolling Background

---
+++ Assets/Scenes/Scripts/ScrollingBackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.y += Time.deltaTime * 9/10;
+        mat.mainTextureOffset = offset;
+
+    }
+}
