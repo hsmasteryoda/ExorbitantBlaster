4b8185a21fde639c312c4b54d48970acc7db53e0
uncagedsoccer <46938673+uncagedsoccer@users.noreply.github.com>
2020-04-25 21:36:38 -0500

    Added In Game Text, End game text does not show up properly

--- Assets/Scenes/Scripts/GameController.cs
+++ Assets/Scenes/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -13,20 +14,41 @@ public class GameController : MonoBehaviour
     public float spawnWait3a;
     public float spawnWait3b;
     public float waveWait;
+    public float textWait;
     public float startWait;
     public GameObject smallAsteroid;
     public int wave3Count;
     private bool wave1 = true;
     private bool wave2 = true;
     private bool wave3 = true;
+    public Text gameOverText;
+    public Text waveText;
+    public Text restartText;
+    public Text congratText;
+    public Text countDownText;
+    private bool gameOver;
 
     void Start()
     {
+        gameOver = false;
+        gameOverText.text = "";
+        restartText.text = "";
+        waveText.text = "";
+        congratText.text = "";
         StartCoroutine(SpawnWaves());
     }
 
     IEnumerator SpawnWaves()
     {
+        countDownText.text = "3";
+        yield return new WaitForSeconds(1);
+        countDownText.text = "2";
+        yield return new WaitForSeconds(1);
+        countDownText.text = "1";
+        yield return new WaitForSeconds(1);
+        countDownText.text = "Start";
+        yield return new WaitForSeconds(1);
+        countDownText.text = "";
         yield return new WaitForSeconds(startWait);
         while (wave1 == true)
         {
@@ -37,7 +59,10 @@ public class GameController : MonoBehaviour
                 Instantiate(smallAsteroid, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait1);
             }
+            yield return new WaitForSeconds(textWait);
+            waveText.text = "wave 2";
             yield return new WaitForSeconds(waveWait);
+            waveText.text = "";
             wave1 = false;
 
         }
@@ -50,6 +75,10 @@ public class GameController : MonoBehaviour
                 Instantiate(bigAsteroid, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait2);
             }
+            yield return new WaitForSeconds(textWait);
+            waveText.text = "Wave 3";
+            yield return new WaitForSeconds(waveWait);
+            waveText.text = "";
             wave2 = false;
         }
         while(wave3 == true)
@@ -63,7 +92,13 @@ public class GameController : MonoBehaviour
                 Instantiate(bigAsteroid, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait3b);
             }
+            yield return new WaitForSeconds(textWait);
+            congratText.text = "Congratulation Your A Winner !!!!!";
         }
     }
-
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
 }
